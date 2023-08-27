36ab39425e844845be8c5651105d18bdf05301a7
drax-uk <6754999+drax-uk@users.noreply.github.com>
2023-08-27 08:59:12 +0100

    main.tf added

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block                     = "172.16.0.0/16"
+  enable_dns_hostnames           = "true"
+  enable_dns_support             = "true"
+}
