5838f7bb38440a8f58b8abbe73053d9d897ada05
Drax <6754999+drax-uk@users.noreply.github.com>
2023-08-29 12:10:01 +0100

    added subnets

--- main.tf
+++ main.tf
@@ -8,3 +8,20 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames           = "true"
   enable_dns_support             = "true"
 }
+
+# Create public subnets1
+    resource "aws_subnet" "public1" {
+    vpc_id                     = aws_vpc.main.id
+    cidr_block                 = "172.16.0.0/24"
+    map_public_ip_on_launch    = true
+    availability_zone          = "eu-west-2a"
+
+}
+
+# Create public subnet2
+    resource "aws_subnet" "public2" {
+    vpc_id                     = aws_vpc.main.id
+    cidr_block                 = "172.16.1.0/24"
+    map_public_ip_on_launch    = true
+    availability_zone          = "eu-west-2b"
+}
