7c950c4bf0b63d2753111faf64d4ca99364afa74
Drax <6754999+drax-uk@users.noreply.github.com>
2023-08-29 16:04:50 +0100

    Refactoring

--- main.tf
+++ main.tf
@@ -1,27 +1,21 @@
-provider "aws" {
-  region = "eu-west-2"
+# Get the list of availability zones
+data "aws_availability_zones" "available" {
+state = "available"
 }
 
 # Create VPC
 resource "aws_vpc" "main" {
-  cidr_block                     = "172.16.0.0/16"
-  enable_dns_hostnames           = "true"
-  enable_dns_support             = "true"
+  cidr_block                     = var.vpc_cidr
+  enable_dns_hostnames           = var.enable_dns_hostnames
+  enable_dns_support             = var.enable_dns_support
 }
 
-# Create public subnets1
-    resource "aws_subnet" "public1" {
-    vpc_id                     = aws_vpc.main.id
-    cidr_block                 = "172.16.0.0/24"
-    map_public_ip_on_launch    = true
-    availability_zone          = "eu-west-2a"
 
-}
-
-# Create public subnet2
-    resource "aws_subnet" "public2" {
-    vpc_id                     = aws_vpc.main.id
-    cidr_block                 = "172.16.1.0/24"
-    map_public_ip_on_launch    = true
-    availability_zone          = "eu-west-2b"
+# Create public subnet1
+resource "aws_subnet" "public" {
+  count = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+  vpc_id = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  cidr_block = cidrsubnet(var.vpc_cidr, 4, count.index)
+  availability_zone =  data.aws_availability_zones.available.names[count.index]
 }
